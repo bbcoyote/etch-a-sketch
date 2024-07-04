4d95537d9bbdd82e612c418f51662ba539f49879
Charles Huffman <huffmancharles14@gmail.com>
2024-07-04 11:31:36 -0500

    add function to obtain a grid when inputing a number into prompt

--- main.js
+++ main.js
@@ -5,12 +5,23 @@ newButton.setAttribute("class", "new-btn");
 newButton.textContent = "New Page";
 btnContainer.appendChild(newButton);
 
-const setCanvas = function () {
-    for (i = 0; i < 16; i++) {
-        const canvas = document.createElement('div')
+//how else can I get this functionality in both scopes without repeating myself?
+const setCanvas = function (grid) {
+    grid = prompt("Enter the value of grid. ex 64 would give you a grid of 64x64");
+    const canvas = document.createElement('div')
+    canvas.setAttribute("class", "canvas");
+    for (i = 0; i < grid * grid; i++) {
+        const canvas = document.createElement("div");
         canvas.setAttribute("class", "canvas");
-        container.appendChild(canvas)
+        const num1 = (480 / grid)
+        const percent = ((num1 / 480) * 100)
+        canvas.style.setProperty("flex", `${percent}%`)
+        canvas.style.setProperty("height", `${percent}%`)
+        container.appendChild(canvas);
     }
+
 }
 
-setCanvas()
+newButton.addEventListener("click", () => {
+    setCanvas()
+})
